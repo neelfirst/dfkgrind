9a7b91020a99263b4a59a6f77ad295f8d93b1422
neelfirst <neelfirst@gmail.com>
2022-03-19 23:46:15 -0700

    testing

--- dfkgrind.py
+++ dfkgrind.py
@@ -9,14 +9,17 @@ from dfk.quest import foraging, fishing # will expand into mining/gardening late
 from dfk.quest.quest import Quest
 from dfk.quest.utils import utils as quest_utils
 
-import json, argparse, sys, time
+import json, argparse, sys, time, logging
 
 KEYFILE_LOCATION = "config/keystore.json"
-GAS_GWEI = 35
-TX_TIMEOUT = 30
+RPC_ADDRESS = "https://api.fuzz.fi"
 
-RPC_SERVER = "https://api.fuzz.fi"
-w3 = Web3(Web3.HTTPProvider(RPC_SERVER))
+w3 = Web3(Web3.HTTPProvider(RPC_ADDRESS))
+
+log_format = '%(asctime)s|%(name)s|%(levelname)s: %(message)s'
+logger = logging.getLogger("DFK-quest")
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stdout)
 
 def make_new_keyfile(path_obj):
   x = getpass(prompt='Paste private key: ')
@@ -40,24 +43,29 @@ def manage_keyfile():
 
 def main(hero_id):
   encrypted_key = manage_keyfile()
-  account_address = encrypted_key['address']
   print('loaded encrypted key')
 
   p = getpass(prompt='Enter passphrase to decrypt and use: ')
+  account_address = None
   try:
-    Account.decrypt(encrypted_key, p)
+    account_address = w3.eth.account.privateKeyToAccount(Account.decrypt(encrypted_key, p)).address
   except:
     print("Bad passphrase. Bailing out.")
     sys.exit(1)
 
-  quest = Quest(RPC_SERVER)
-  quest.start_quest(fishing.QUEST_CONTRACT_ADDRESS, \
-                     hero_id, \
-                     1, \
-                     str(Account.decrypt(encrypted_key, p)), \
-                     w3.eth.getTransactionCount(account_address), \
-                     GAS_GWEI, \
-                     TX_TIMEOUT)
+  if not account_address:
+    print("Could not decode checksum-enabled account address. Bailing out.")
+    sys.exit(1)
+
+  quest = Quest(rpc_address=RPC_ADDRESS, logger=logger)
+  quest.start_quest(quest_address=fishing.QUEST_CONTRACT_ADDRESS, \
+                    hero_ids=hero_id, \
+                    attempts=1, \
+                    private_key=str(Account.decrypt(encrypted_key, p)), \
+                    nonce=w3.eth.getTransactionCount(account_address), \
+                    gas_price_gwei=35, \
+                    tx_timeout=30, \
+                    rpc_address=RPC_ADDRESS)
 
   quest_info = quest_utils.human_readable_quest(quest.get_hero_quest(hero_id))
 
@@ -65,13 +73,14 @@ def main(hero_id):
   while time.time() < quest_info['completeAtTime']:
     time.sleep(2)
 
-  tx_receipt = quest.complete_quest(hero_id, \
-                                    str(Account.decrypt(encrypted_key, p)), \
-                                    w3.eth.getTransactionCount(account_address), \
-                                    GAS_GWEI, \
-                                    TX_TIMEOUT)
+  tx_receipt = quest.complete_quest(hero_ids=hero_id, \
+                                    private_key=str(Account.decrypt(encrypted_key, p)), \
+                                    nonce=w3.eth.getTransactionCount(account_address), \
+                                    gas_price_gwei=35, \
+                                    tx_timeout=30, \
+                                    rpc_address=RPC_ADDRESS)
   quest_result = quest.parse_complete_quest_receipt(tx_receipt)
-  logger.info("Rewards: " + str(quest_result))
+  print("Rewards: " + str(quest_result))
 
   return
 
