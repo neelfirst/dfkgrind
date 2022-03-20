163b99c3f839a3f1cb1faeeba900e168068a5350
neelfirst <neelfirst@gmail.com>
2022-03-19 22:14:36 -0700

    minor refactor on key handling

--- dfkgrind.py
+++ dfkgrind.py
@@ -9,7 +9,7 @@ def make_new_keyfile(path_obj):
   with path_obj.open('w') as f:
     f.write(json.dumps(z))
   print('wrote encrypted keystore to config/keystore.json')
-  return
+  return z
 
 def manage_keyfile():
   key = pathlib.Path('config/keystore.json')
@@ -17,13 +17,13 @@ def manage_keyfile():
     print('encrypted keystore file found, loading...')
     with key.open() as f:
       encrypted_key = json.loads(f.read())
-    print(encrypted_key)
   else:
     print('keystore not found, making new key. get your MM creds ready')
-    make_new_keyfile(key)
+    encrypted_key = make_new_keyfile(key)
+  return encrypted_key
 
 def main():
-  manage_keyfile()
+  encrypted_key = manage_keyfile()
 
 if __name__ == '__main__':
   main()
