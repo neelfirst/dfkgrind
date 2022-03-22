8c11741c10721202296464219c83217639a71f4e
neelfirst <neelfirst@gmail.com>
2022-03-21 19:58:30 -0700

    logging refactoring

--- item.py
+++ item.py
@@ -2,6 +2,7 @@
 # usage: use_item(hero_id, Account.decrypt(encrypted_key, p))
 
 from web3 import Web3
+import logging
 
 # consumeItem contract address
 CONTRACT_ADDRESS = '0x38e76972bd173901b5e5e43ba5cb464293b80c31'
@@ -50,16 +51,15 @@ def use_item(hero_id, private_key, gas_price_gwei=35, tx_timeout_seconds=30, rpc
   tx = contract.functions.consumeItem(ITEM_CONTRACT_ADDRESS, hero_id).buildTransaction(
     {'gasPrice': w3.toWei(gas_price_gwei, 'gwei'), 'nonce': nonce})
 
-  print("Signing transaction")
+  logger = logging.getLogger('dfkgrind')
+  logger.info("Signing transaction")
   signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
-  print("Sending transaction " + str(tx))
+  logger.info("Sending transaction " + str(tx))
   ret = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-  print("Transaction successfully sent !")
-  print(
-    "Waiting for transaction " + block_explorer_link(signed_tx.hash.hex()) + " to be mined")
+  logger.info("Transaction successfully sent !")
+  logger.info("Waiting for transaction " + block_explorer_link(signed_tx.hash.hex()) + " to be mined")
 
   tx_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash=signed_tx.hash, timeout=tx_timeout_seconds,
                                                      poll_latency=3)
-  print("Transaction mined !")
-
+  logger.info("Transaction mined !")
   return tx_receipt
